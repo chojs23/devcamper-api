1b26cf27f55a64411dc561149fa50f31e89aebac
neo <chojs990222@gmail.com>
2022-11-30 01:18:13 +0900

    Initial express setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["airbnb", "prettier"],
+  plugins: ["plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+};
