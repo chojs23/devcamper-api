a0ed7cc3b38c90dca0641b0adc6e1e964c5e7ef4
neo <chojs990222@gmail.com>
2022-12-01 02:11:08 +0900

    photo upload

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,13 +6,15 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  bootcampPhotoUpload,
 } = require("../controllers/bootcamps");
-
 // Include other resource routers
 const courseRouter = require("./courses");
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
+
+router.route("/:id/photo").put(bootcampPhotoUpload);
 
 router.route("/").get(getBootcamps).post(createBootcamp);
 
