3c25e422b1031427ba5f0579d8110d849a9e9609
neo <chojs990222@gmail.com>
2022-12-02 02:16:22 +0900

    Update User Details/Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,6 +62,43 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc   Update user details
+// @route  PUT /api/v1/auth/updatedetails
+// @access Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc   Update password
+// @route  PUT /api/v1/auth/updatepassword
+// @access Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc   Forgot password
 // @route  POST /api/v1/auth/forgotpassword
 // @access Public
@@ -75,7 +112,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // Get reset token
   const resetToken = user.getResetPasswordToken();
 
-  await user.save({ isNew: false });
+  await user.save({ validateBeforeSave: false });
 
   // Create reset url
   const resetUrl = `${req.protocol}://${req.get(
@@ -97,7 +134,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     user.resetPasswordToken = undefined;
     user.resetPasswordExpire = undefined;
 
-    await user.save({ isNew: false });
+    await user.save({ validateBeforeSave: false });
 
     return next(new ErrorResponse("Email could not be sent", 500));
   }
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const { protect } = require("../middleware/auth");
@@ -13,6 +15,8 @@ const { protect } = require("../middleware/auth");
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/me").get(protect, getMe);
+router.route("/updatedetails").put(protect, updateDetails);
+router.route("/updatepassword").put(protect, updatePassword);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/resetpassword/:resettoken").put(resetPassword);
 
