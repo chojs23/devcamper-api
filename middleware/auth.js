345d9d1f41dbdb254c0c2893233670f97166a95d
neo <chojs990222@gmail.com>
2022-12-01 23:39:55 +0900

    Role Authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,7 +22,6 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   try {
     // Verify token
-    console.log(token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = await User.findById(decoded.id);
@@ -32,3 +31,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
